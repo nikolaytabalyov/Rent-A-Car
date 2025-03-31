e20a51abfc3fc8c156c02a36adb05aa6b0074e52
DaniDagami <daniel.g.boychev@gmail.com>
2025-03-31 15:45:17 +0300

    Models

---
+++ RentACar/RentACar/Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace RentACar.Models {
+    public class Car {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int PassengerSeats { get; set; }
+        public string Description { get; set; }
+        public decimal PricePerDay { get; set; }
+    }
+}
---
+++ RentACar/RentACar/Models/Reservation.cs
@@ -0,0 +1,9 @@
+﻿namespace RentACar.Models {
+    public class Reservation {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public int UserId { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
